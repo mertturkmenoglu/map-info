5463a9dac66fa555c9f8174133028aaf806dcead
Mert <mertturkmenoglu99@gmail.com>
2020-01-31 22:01:00 +0300

    Add Leaflet map

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 window.onload = () => {
-    console.log('Window onload');
+    const m = L.map('geoMap').setView([0, 0], 3);
+    const attr = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
+
+    const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    const tiles = L.tileLayer(tileUrl, { attr });
+    tiles.addTo(m);
+
+    m.on('click', e => console.log(`Clicked to: ${e.latlng}`));
 };
