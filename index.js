58238bbebdd707479871233d7c1ace10f43e2344
Mert <mertturkmenoglu99@gmail.com>
2020-02-01 19:28:30 +0300

    Fix favicon

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const fetch = require('node-fetch');
+const favicon = require('serve-favicon');
 const app = express();
 require('dotenv').config();
 
@@ -8,6 +9,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.png')));
 app.use(express.json());
 app.use(express.text());
 
